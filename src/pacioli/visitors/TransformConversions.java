3add61baf22dab0ca0db676ddb4b0eef61c7b432
pgriffel <pgriffel@gmail.com>
2020-03-24 14:18:43 +0100

    Some cleaning in new unit conversion code

--- src/pacioli/visitors/TransformConversions.java
+++ src/pacioli/visitors/TransformConversions.java
@@ -3,11 +3,7 @@ package pacioli.visitors;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.sql.rowset.RowSetMetaDataImpl;
-
-import mvm.values.matrix.MatrixBase;
 import pacioli.Location;
-import pacioli.Pacioli;
 import pacioli.PacioliException;
 import pacioli.ast.IdentityTransformation;
 import pacioli.ast.Visitor;
@@ -15,15 +11,12 @@ import pacioli.ast.expression.ConversionNode;
 import pacioli.ast.expression.IdentifierNode;
 import pacioli.ast.expression.MatrixLiteralNode;
 import pacioli.ast.expression.MatrixLiteralNode.ValueDecl;
-import pacioli.types.PacioliType;
 import pacioli.types.TypeBase;
 import pacioli.types.matrix.MatrixType;
 import pacioli.types.matrix.VectorBase;
 import uom.DimensionedNumber;
 import uom.Fraction;
-import uom.Unit;
 import uom.UnitFold;
-import uom.UnitMap;
 
 /**
  * Transforms all conversion nodes into matrix literal nodes with the proper
@@ -43,13 +36,7 @@ public class TransformConversions extends IdentityTransformation implements Visi
         
         MatrixType type = (MatrixType) node.typeNode.evalType(true);
         
-        //node.rowDim = compileTimeMatrixDimension(type.rowDimension);
-        //node.columnDim = compileTimeMatrixDimension(matrixType.columnDimension);
-        
-        //DimensionedNumber<TypeBase> typeFactor = new DimensionedNumber<TypeBase>(type.factor);
         DimensionedNumber<TypeBase> typeFactor = type.factor.flat();
-        
-        Pacioli.logln("conv type: %s  factor=%s", type.pretty(), typeFactor);
         
         if (!type.rowDimension.equals(type.columnDimension)) {
             throw new RuntimeException("Invalid conversion", 
@@ -68,18 +55,7 @@ public class TransformConversions extends IdentityTransformation implements Visi
             
             for (int i = 0; i < nrItems; i++) {
                 final List<String> items = node.rowDim.ElementAt(i);
-                //final int[] positions = node.rowDim.individualPositions(i);
                 assert(items.size() == width);
-                //for (int d = 0; d < width; d++) {
-                    UnitMap<TypeBase> mapper = new UnitMap<TypeBase>() {
-                        @Override
-                        public Unit<TypeBase> map(TypeBase base) {
-                            VectorBase vbase = (VectorBase) base;
-                            String itemName = items.get(vbase.position);
-                             Unit<TypeBase> unit = vbase.vectorUnitInfo.getUnit(itemName).unit();
-                            return unit;
-                        }
-                    };
                     UnitFold<TypeBase, DimensionedNumber<TypeBase>> folder = 
                             new UnitFold<TypeBase, DimensionedNumber<TypeBase>>() {
 
@@ -108,8 +84,6 @@ public class TransformConversions extends IdentityTransformation implements Visi
                                 }
                         
                     };
-                    //Unit<TypeBase> rowUnit = type.rowUnit.map(mapper);
-                    //Unit<TypeBase> columnsUnit = type.columnUnit.map(mapper);
                     
                     DimensionedNumber<TypeBase> rowUnit = type.rowUnit.fold(folder);
                     DimensionedNumber<TypeBase> columnsUnit = type.columnUnit.fold(folder);
@@ -117,9 +91,9 @@ public class TransformConversions extends IdentityTransformation implements Visi
                     DimensionedNumber<TypeBase> div = 
                             typeFactor.multiply(rowUnit.multiply(columnsUnit.reciprocal()));
                     DimensionedNumber<TypeBase> flat = div.flat();
-                    Pacioli.logln("CONV %s -> %s  = %s", rowUnit.toText(), columnsUnit.toText(),
-                            flat.factor().toPlainString());
                 
+                    // todo: how to handle empty dimension?
+                    
                     List<IdentifierNode> key = new ArrayList<IdentifierNode>();
                     for (String item: items) {
                         key.add(new IdentifierNode(item, node.getLocation()));    
@@ -131,7 +105,6 @@ public class TransformConversions extends IdentityTransformation implements Visi
                     String value = flat.factor().toPlainString();
                     
                     conversionFactors.add(new ValueDecl(key, value));
-                //}
             }
             
             // Create a literal node of the same type with the conversion factors.
