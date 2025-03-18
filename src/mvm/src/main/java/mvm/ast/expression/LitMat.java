49d287efe8b4ea35e7fc37f58065c74989d7a2ad
pgriffel <pgriffel@gmail.com>
2025-03-18 23:42:28 +0100

    Move parsing in mvm literal node to constructor

--- src/mvm/src/main/java/mvm/ast/expression/LitMat.java
+++ src/mvm/src/main/java/mvm/ast/expression/LitMat.java
@@ -1,6 +1,7 @@
 package mvm.ast.expression;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 import mvm.Environment;
@@ -12,24 +13,29 @@ import mvm.values.matrix.Matrix;
 public class LitMat implements Expression {
 
     private ShapeNode shape;
-    private List<String> rows;
-    private List<String> columns;
-    private List<String> values;
+    private List<Integer> rows;
+    private List<Integer> columns;
+    private List<Double> values;
 
     public LitMat(ShapeNode shape, List<String> rows, List<String> columns, List<String> values) {
         this.shape = shape;
-        this.rows = rows;
-        this.columns = columns;
-        this.values = values;
+        this.rows = new ArrayList<>();
+        this.columns = new ArrayList<>();
+        this.values = new ArrayList<>();
+        for (int i = 0; i < values.size(); i++) {
+            this.rows.add(Integer.parseInt(rows.get(i)));
+            this.columns.add(Integer.parseInt(columns.get(i)));
+            this.values.add(Double.parseDouble(values.get(i)));
+        }
     }
 
     @Override
     public PacioliValue eval(Environment environment) throws MVMException {
         Matrix matrix = new Matrix(shape.eval(environment.machine()));
         for (int i = 0; i < values.size(); i++) {
-            Integer r = Integer.parseInt(rows.get(i));
-            Integer c = Integer.parseInt(columns.get(i));
-            Double value = Double.parseDouble(values.get(i));
+            Integer r = rows.get(i);
+            Integer c = columns.get(i);
+            Double value = values.get(i);
             matrix.set(r, c, value);
         }
         return matrix;
