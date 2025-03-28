1005a3d2a7dfc8300fad8efa3004ad4df97492d5
pgriffel <pgriffel@gmail.com>
2025-03-28 01:28:23 +0100

    Updated pacioli src licenses

--- src/pacioli/src/main/java/pacioli/Pacioli.java
+++ src/pacioli/src/main/java/pacioli/Pacioli.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 - 2014 Paul Griffioen
+ * Copyright (c) 2013 - 2025 Paul Griffioen
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -18,6 +18,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 package pacioli;
 
 import java.io.BufferedWriter;
--- src/pacioli/src/main/java/pacioli/compiler/Bundle.java
+++ src/pacioli/src/main/java/pacioli/compiler/Bundle.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2013 - 2025 Paul Griffioen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package pacioli.compiler;
 
 import java.io.File;
@@ -14,7 +35,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import pacioli.Pacioli;
-import pacioli.ast.definition.Declaration;
 import pacioli.ast.definition.Definition;
 import pacioli.ast.definition.Toplevel;
 import pacioli.ast.definition.ValueDefinition;
@@ -25,7 +45,6 @@ import pacioli.ast.visitors.JSGenerator;
 import pacioli.ast.visitors.MVMGenerator;
 import pacioli.ast.visitors.MatlabGenerator;
 import pacioli.ast.visitors.PythonGenerator;
-import pacioli.ast.visitors.AllIdentifiersVisitor;
 import pacioli.ast.visitors.AllIdentifiersVisitor.IdentifierInfo;
 import pacioli.compiler.CompilationSettings.Target;
 import pacioli.symboltable.PacioliTable;
