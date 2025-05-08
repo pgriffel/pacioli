6b05c934d09bce60af97b9dde6549e3de69d0c0c
pgriffel <pgriffel@gmail.com>
2025-05-09 01:33:54 +0200

    Adding labels and text to space

--- src/pacioli-js/src/space.ts
+++ src/pacioli-js/src/space.ts
@@ -47,7 +47,8 @@ export type PacioliScene = [
   PacioliMesh[],
   PacioliPath[],
   PacioliSpotLight[],
-  AmbientLight
+  AmbientLight,
+  PacioliLabel[]
 ];
 
 /**
@@ -109,6 +110,17 @@ type PacioliSpotLight = [
 type AmbientLight = [
   PacioliString, // color
   PacioliMatrix // intensity
+];
+
+/**
+ * Matches the Label type from the graphics Pacioli library.
+ */
+type PacioliLabel = [
+  PacioliString, // characters
+  PacioliMatrix, // position
+  PacioliMatrix, // direction
+  PacioliString, // color
+  PacioliString // font
 ];
 
 /**
@@ -469,7 +481,7 @@ export class Space {
     this.clear();
 
     // Add all scene elements
-    const [name, vectors, meshes, paths, lights, ambientLight] = scene;
+    const [name, vectors, meshes, paths, lights, ambientLight, labels] = scene;
     for (const mesh of meshes) {
       this.addMesh(mesh);
     }
@@ -481,6 +493,9 @@ export class Space {
     }
     for (const [position, target, color, intensity] of lights) {
       this.addSpotLight(position, target, color, intensity);
+    }
+    for (const [characters, position, direction, color, font] of labels) {
+      this.addLabel(characters, position, direction, color, font);
     }
 
     // Don't overrule light that is set via options. This allows
@@ -901,6 +916,31 @@ export class Space {
 
     this.body.add(light);
     this.body.add(light.target);
+  }
+
+  private addLabel(
+    characters: PacioliString,
+    position: PacioliMatrix,
+    direction: PacioliMatrix,
+    color: PacioliString,
+    font: PacioliString
+  ) {
+    console.log("Adding label", characters, position, direction, color, font);
+
+    const label = newLabel(characters.value, 0.5);
+
+    if (label) {
+      label.position.x = 0;
+      label.position.y = 5;
+      label.position.z = 0;
+
+      this.body.add(label);
+    } else {
+      console.log("no label to add");
+    }
+    // this.body.add(arrowLabel);
+
+    console.log("Added label", characters, position, direction, color, font);
   }
 
   // TODO: updatePath
@@ -1372,4 +1412,58 @@ function vec2String(vector: PacioliMatrix) {
     1,
     0
   ).toFixed(5)}, ${getNumber(vector.numbers, 2, 0).toFixed(5)})`;
+}
+
+function newLabel(
+  text: string,
+  size: number
+): THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial> {
+  // Write the text on a new canvas.
+  var canvas = document.createElement("canvas");
+  var context = canvas.getContext("2d");
+  if (context) {
+    context.font = "256px sans-serif";
+
+    // measureText gives css pixels. 2.54cm = 96px
+    var width = context.measureText(text).width;
+    var height = 256;
+
+    console.log("width = ", width);
+
+    // Create a picture of the text. It must be square to maintain
+    // size ratios and large enough to hold the text. Assume the width
+    // is larger than the height.
+    canvas.width = width;
+    canvas.height = height;
+
+    context.font = "256px sans-serif";
+    context.textBaseline = "top";
+    context.fillText(text, 0, 0);
+
+    // Put the canvas in a texture and make a plane.
+    var texture = new THREE.CanvasTexture(canvas);
+
+    texture.minFilter = THREE.LinearFilter;
+    texture.wrapS = THREE.ClampToEdgeWrapping;
+    texture.wrapT = THREE.ClampToEdgeWrapping;
+
+    texture.needsUpdate = true;
+    var cover = new THREE.MeshBasicMaterial({
+      map: texture,
+      transparent: true,
+      side: THREE.DoubleSide,
+    });
+
+    var scale = size / 256;
+    // var shape = new THREE.PlaneGeometry(width, height);
+    var shape = new THREE.PlaneGeometry(width * scale, height * scale);
+
+    // Create the label
+    var label = new THREE.Mesh(shape, cover);
+    // label.needsUpdate = true;
+
+    return label;
+  } else {
+    throw new Error("no context for label");
+  }
 }
