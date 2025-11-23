f30c386877c8b7a6aa37574d98706214bf6d474f
pgriffel <pgriffel@gmail.com>
2025-11-23 21:14:10 +0100

    Updated licence headers

--- src/mvm/src/main/java/mvm/values/TheVoid.java
+++ src/mvm/src/main/java/mvm/values/TheVoid.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 - 2015 Paul Griffioen
+ * Copyright (c) 2013 - 2025 Paul Griffioen
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
