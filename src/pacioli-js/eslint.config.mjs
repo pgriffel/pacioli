c3aec14eda3add985c4de0248a1b67a299e65138
pgriffel <pgriffel@gmail.com>
2025-12-28 23:26:10 +0100

    Improved eslint config

--- src/pacioli-js/eslint.config.mjs
+++ src/pacioli-js/eslint.config.mjs
@@ -1,18 +1,35 @@
 // @ts-check
 
 import eslint from "@eslint/js";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
+export default defineConfig([
+  globalIgnores([
+    "src/linear-algebra",
+    "test",
+    "dist",
+    "esbuild.js",
+    "esbundle.js",
+    "*.js",
+  ]),
   {
-    ignores: ["test/**"],
+    // ignores: ["test/**", "src/linear-algebra/**"],
     rules: {
       "@typescript-eslint/no-explicit-any": "warn",
+
       "@typescript-eslint/consistent-type-imports": "warn",
       "@typescript-eslint/no-import-type-side-effects": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+        },
+      ],
     },
-  }
-);
+    extends: [eslint.configs.recommended, tseslint.configs.recommended],
+  },
+]);
