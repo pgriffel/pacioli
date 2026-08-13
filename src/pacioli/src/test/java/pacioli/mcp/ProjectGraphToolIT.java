1190d12babaf700a18f3b8cdadd6fbbc0e66b0a4
pgriffel <pgriffel@gmail.com>
2026-08-13 02:26:32 +0200

    Fixed the project graph test

--- src/pacioli/src/test/java/pacioli/mcp/ProjectGraphToolIT.java
+++ src/pacioli/src/test/java/pacioli/mcp/ProjectGraphToolIT.java
@@ -83,7 +83,7 @@ class ProjectGraphToolIT {
         // Then the text should be correct
         String text = contents0.get("text").getAsString();
         int hash = text.hashCode();
-        assertTrue(hash == 681680283 || hash == 10900128);
+        assertTrue(hash == 681680283 || hash == -842510917);
 
         // Teardown
         server.stop();
